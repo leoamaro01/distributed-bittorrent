ffb6201460c7e3662412d712fa56f0c7ae775267
Campione <80720402+alfrecampione@users.noreply.github.com>
2024-07-17 04:01:22 -0400

    Update texts.py

--- tracker/texts.py
+++ tracker/texts.py
@@ -1,8 +1,32 @@
 help_text = """Commands in the CDL-BitTorrent Server CLI:
 
-    help         Displays this page.
-    delete       Deletes a torrent from the server.
-    exit         Closes the server.
+    help           Displays this page.
+    delete         Deletes a torrent from the server.
+    list-torrents  Displays the IDs of every torrent currently available.
+    list-peers     Displays the IP address of every peer online.
+    info           Displays info on a particular torrent, including seeders.
+    exit           Closes the server.
+"""
+
+list_torrents_help = """Displays the IDs of every torrent currently available.
+
+Usage:
+    list-torrents       Lists every available torrent.
+    list-torrents help  Displays this page.
+"""
+
+list_peers_help = """Displays the IP address of every peer currently online.
+
+Usage:
+    list-peers       Lists online peers.
+    list-peers help  Displays this page.
+"""
+
+info_help = """Displays info on a particular torrent, including seeders.
+
+Usage:
+    info [TORRENT ID]  Displays torrent info.
+    info help          Displays this page.
 """
 
 delete_help = """Deletes a torrent from the server.
